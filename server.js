ef4d839d8058c41e9f0ca3af196c125e19ec1aae
VedshreeB <vedshree74@gmail.com>
2022-11-11 15:09:33 +0530

    c

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.use((error, req, res, next) => {
     })
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 mongoose.connect(process.env.MONGO_URL.replace("<password>", process.env.MONGO_PASSWORD)
     , { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true,useFindAndModify: false }).then(() => {
